da5882eca03ff7fbd60597857235192628f2cfc8
Jordan Carlin <jordanmcarlin@gmail.com>
2024-09-02 18:33:37 -0700

    Move source to fix .gitignore issues

---
+++ fpga/radiant_project/e155_lab1/source/lab1_jc.sv
@@ -0,0 +1,18 @@
+// lab1_jc.sv
+// Jordan Carlin, jcarlin@hmc.edu, 2 September 2024
+// FPGA implementation for E155 Lab 1
+
+module lab1_jc (
+	input  logic [3:0] s,
+  output logic [2:0] led,
+  output logic [6:0] segs
+);
+
+	logic clk;
+
+	// Internal high-speed oscillator
+	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(clk));
+
+	// Main verilog module
+	top top (.clk, .s, .led, .segs);
+endmodule
